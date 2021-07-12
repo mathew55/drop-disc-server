da260610eb708728e5a40f79da91eb021437bf79
Mathew Kuriakose <kuriakosemathew007@gmail.com>
2021-07-12 18:37:05 +0100

    api end points standardized

--- app/controller/game_controller.py
+++ app/controller/game_controller.py
@@ -1,18 +1,23 @@
 from flask import Blueprint
+from flask import request
 from app.services.move_manager import next_board_move
 from app.services.move_manager import next_player
 from app.model.game_models.player import Player
-from flask import request
 from app.model.response_models.response import Response
 from app.context.app_context import ApplicationContext
 from app.services.player_manager import PlayerMatcher
+from app.model.conf import Config
 
-
+config = Config()
+baseEndpoint = config.get_config_value("endpoint", "base")
 game_controller = Blueprint('game_controller', __name__, template_folder="templates")
 cx = ApplicationContext.getContext()
 conn = PlayerMatcher()
 
-@game_controller.route('/start-game/', methods= ["POST"])
+
+#TODO Add validation to check if the request is of application/json type
+
+@game_controller.route(f'{baseEndpoint}/startgame/', methods= ["POST"])
 def start_game():
     player_name = request.args.get("player_name")
     player_token = int(request.args.get("player_token"))
@@ -27,12 +32,13 @@ def start_game():
         code=200
     ).response()
 
-@game_controller.route('/next-move/', methods= ["POST"])
+
+@game_controller.route(f'{baseEndpoint}/nextmove/', methods= ["POST"])
 def make_move():
     game_id = request.args.get("game_id")
     move_col = int(request.args.get("move_col"))
 
-    print(f"Move_COl - {move_col}")
+    print(f"Move_Col - {move_col}")
     game = next_board_move(game_id, move_col)
 
     return Response(
@@ -41,8 +47,9 @@ def make_move():
         code=200
     ).response()
 
-@game_controller.route('/get-player/', methods= ["POST"])
-def current_player():
+
+@game_controller.route(f'{baseEndpoint}/getgamestate/', methods= ["POST"])
+def get_game_state():
     game_id = request.args.get("game_id")
     print(f"GameId is {game_id}")
     game = next_player(game_id)
--- app/controller/healthcheck_controller.py
+++ app/controller/healthcheck_controller.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 from app.model.response_models.health_check_response import HealthCheck
+from app.model.conf import Config
 
 health_check_ep = Blueprint('health_check_ep', __name__, template_folder="templates")
+config = Config()
+baseEndpoint = config.get_config_value("endpoint", "base")
 
-@health_check_ep.route('/ping', methods=['GET'])
+@health_check_ep.route(f'{baseEndpoint}/ping', methods=['GET'])
 def check_health():
     """Determins if the container is working and healthy. Returns a JSON object with relevant data regarding the health of the service"""
     health_check = HealthCheck()
